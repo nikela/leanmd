82d6cfc2c7d555227008613d628440b03e49be95
Nikhil Jain <nikhil@illinois.edu>
2011-10-09 01:12:03 -0500

    Done with main files and mol3d (except the configurable paramters)

--- Main.h
+++ Main.h
@@ -1,15 +1,5 @@
-/** \file Main.h
- *  Author: Abhinav S Bhatele
- *  Date Created: August 11th, 2008
- */
-
-#include "SimParameters.h"
-#include "Parameters.h"
-
 #ifndef __MAIN_H__
 #define __MAIN_H__
-
-PUPbytes(SimParameters)
 
 /** \class Main
  *
@@ -17,10 +7,6 @@ PUPbytes(SimParameters)
 class Main : public CBase_Main {
   private:
     int phase;
-    const char* structureFilename;
-    const StringList* sl_parameters;
-    SimParameters *simParams; //not actually the full simparameters class from namd
-    Parameters *fileParams;    
 
   public:
     Main(CkArgMsg* msg);
@@ -28,13 +14,8 @@ class Main : public CBase_Main {
     void pup(PUP::er &p) {
       Chare::pup(p);
       p|phase;
-      if (p.isUnpacking())  simParams = new SimParameters;
-      p|*simParams;
     }
 
-    void readConfigFile(const char* filename);
-    void readParameterFile(const StringList* sl_params, SimParameters* sParams);
-    FileDataMsg* readParticleData();
     void allDone();
     void lbBarrier();
     void ftBarrier();
