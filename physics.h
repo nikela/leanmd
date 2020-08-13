46c640e8e7f0805da0f0ff24b179247332e3fb94
Jaemin Choi <jchoi157@illinois.edu>
2020-08-13 18:44:12 -0400

    Clean up physics.h

--- physics.h
+++ physics.h
@@ -3,9 +3,9 @@
 
 #include "defs.h"
 
-#define BLOCK_SIZE	512
+#define BLOCK_SIZE 512
 
-//function to calculate forces among 2 lists of atoms
+// Function to calculate forces among 2 lists of atoms
 inline double calcPairForces(ParticleDataMsg* first, ParticleDataMsg* second, int stepCount,
                              std::vector<vec3>& force1, std::vector<vec3> &force2) {
   int i, j, ptpCutOffSqd, diff;
@@ -16,42 +16,44 @@ inline double calcPairForces(ParticleDataMsg* first, ParticleDataMsg* second, in
   double rSix, rTwelve;
   double energy = 0;
   int doEnergy = 0;
-  if(stepCount == 1 || stepCount == finalStepCount)
+  if (stepCount == 1 || stepCount == finalStepCount) {
     doEnergy = 1;
+  }
 
   force1.resize(firstLen);
   force2.resize(secondLen);
-  //check for wrap around and adjust locations accordingly
-  if (abs(first->x - second->x) > 1){
+
+  // Check for wrap around and adjust locations accordingly
+  if (abs(first->x - second->x) > 1) {
     diff = CELL_SIZE_X * cellArrayDimX;
     if (second->x < first->x)
-      diff = -1 * diff; 
+      diff = -1 * diff;
     for (i = 0; i < firstLen; i++)
       first->part[i].x += diff;
   }
-  if (abs(first->y - second->y) > 1){
+  if (abs(first->y - second->y) > 1) {
     diff = CELL_SIZE_Y * cellArrayDimY;
     if (second->y < first->y)
-      diff = -1 * diff; 
+      diff = -1 * diff;
     for (i = 0; i < firstLen; i++)
       first->part[i].y += diff;
   }
-  if (abs(first->z - second->z) > 1){
+  if (abs(first->z - second->z) > 1) {
     diff = CELL_SIZE_Z * cellArrayDimZ;
     if (second->z < first->z)
-      diff = -1 * diff; 
+      diff = -1 * diff;
     for (i = 0; i < firstLen; i++)
       first->part[i].z += diff;
-  } 
+  }
   ptpCutOffSqd = PTP_CUT_OFF * PTP_CUT_OFF;
   powTen = pow(10.0, -10);
   powTwenty = pow(10.0, -20);
 
   int i1, j1;
-  for(i1 = 0; i1 < firstLen; i1=i1+BLOCK_SIZE)
-    for(j1 = 0; j1 < secondLen; j1=j1+BLOCK_SIZE)
-      for(i = i1; i < i1+BLOCK_SIZE && i < firstLen; i++) {
-        for(j = j1; j < j1+BLOCK_SIZE && j < secondLen; j++) {
+  for (i1 = 0; i1 < firstLen; i1=i1+BLOCK_SIZE) {
+    for (j1 = 0; j1 < secondLen; j1=j1+BLOCK_SIZE) {
+      for (i = i1; i < i1+BLOCK_SIZE && i < firstLen; i++) {
+        for (j = j1; j < j1+BLOCK_SIZE && j < secondLen; j++) {
           separation = first->part[i] - second->part[j];
           rsqd = dot(separation, separation);
           if (rsqd > 1 && rsqd < ptpCutOffSqd) {
@@ -60,8 +62,9 @@ inline double calcPairForces(ParticleDataMsg* first, ParticleDataMsg* second, in
             rSix = ((double)rsqd) * rsqd * rsqd;
             rTwelve = rSix * rSix;
             f = (double)( (12 * VDW_A) / rTwelve - (6 * VDW_B) / rSix);
-            if(doEnergy)
-              energy += (double)( VDW_A / rTwelve - VDW_B / rSix); // in milliJoules
+            if (doEnergy) {
+              energy += (double)( VDW_A / rTwelve - VDW_B / rSix); // In milliJoules
+            }
             fr = f / rsqd;
             force = separation * (fr * powTen);
             force1[i] += force;
@@ -69,11 +72,13 @@ inline double calcPairForces(ParticleDataMsg* first, ParticleDataMsg* second, in
           }
         }
       }
+    }
+  }
 
   return energy;
 }
 
-//function to calculate forces among atoms in a single list
+// Function to calculate forces among atoms in a single list
 inline double calcInternalForces(ParticleDataMsg* first, int stepCount, std::vector<vec3>& force1) {
   int i, j, ptpCutOffSqd;
   int firstLen = first->lengthAll;
@@ -82,27 +87,30 @@ inline double calcInternalForces(ParticleDataMsg* first, int stepCount, std::vec
   double rSix, rTwelve;
   double energy = 0;
   int doEnergy = 0;
-  if(stepCount == 1 || stepCount == finalStepCount)
+  if (stepCount == 1 || stepCount == finalStepCount) {
     doEnergy = 1;
+  }
   force1.resize(firstLen);
 
   ptpCutOffSqd = PTP_CUT_OFF * PTP_CUT_OFF;
   powTen = pow(10.0, -10);
   powTwenty = pow(10.0, -20);
-  for(i = 0; i < firstLen; i++){
+
+  for (i = 0; i < firstLen; i++) {
     firstpos = first->part[i];
-    for(j = i+1; j < firstLen; j++) {
-      // computing base values
+    for (j = i+1; j < firstLen; j++) {
+      // Computing base values
       separation = firstpos - first->part[j];
       rsqd = dot(separation, separation);
-      if(rsqd > 1 && rsqd < ptpCutOffSqd){
+      if (rsqd > 1 && rsqd < ptpCutOffSqd){
         rsqd = rsqd * powTwenty;
         r = sqrt(rsqd);
         rSix = ((double)rsqd) * rsqd * rsqd;
         rTwelve = rSix * rSix;
         f = (double)( (12 * VDW_A) / rTwelve - (6 * VDW_B) / rSix);
-        if(doEnergy)
+        if (doEnergy) {
           energy += (double)( VDW_A / rTwelve - VDW_B / rSix);
+        }
 
         fr = f / rsqd;
         force = separation * (fr * powTen);
@@ -111,6 +119,7 @@ inline double calcInternalForces(ParticleDataMsg* first, int stepCount, std::vec
       }
     }
   }
+
   return energy;
 }
 #endif
